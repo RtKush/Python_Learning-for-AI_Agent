c9055e74490587a7d3143b23e6d5aa06ea2fea44
RtKush <govindgupta7947@gmail.com>
2026-01-13 12:51:18 +0530

    basic python programs

---
+++ split.py
@@ -0,0 +1,108 @@
+# str="as"
+# print(str.split('a'))
+
+# str="I am learning python"
+# print(str.split())
+
+
+# str="I am learning python"
+# print(str.split('l'))
+
+# import subprocess
+# import os
+
+# # Create a custom environment
+# env = os.environ.copy()  # Start with the current environment
+# env['CUSTOM_VAR'] = 'value'  # Add or modify variables
+
+# # Run a command with the custom environment
+# result = subprocess.rstr="I am learning python"
+# # print(str.split('l'))
+# un(['echo', '$CUSTOM_VAR'], env=env, shell=True, capture_output=True, text=True)
+# print(result.stdout)
+
+# # Specify a working directory
+# result = subprocess.run(['ls', '-la'], cwd='/tmp', capture_output=True, text=True)
+# print(result.stdout)
+
+#Basic data types
+# name="kush"
+# roll=23
+# per=89.9
+# is_ok=True
+# print(name)
+# print(roll)
+# print(per)
+# print(is_ok)
+
+
+# # Cheacking types
+# print(type(name))
+# print(type(roll))
+# print(type(per))
+# print(type(is_ok))
+
+# #input
+# name=input("Enter name : ")
+# roll=int(input("Enter roll number : "))
+# per=float(input("Enter ur Percentage"))
+
+# print(name)
+# print(roll)
+# print(per)
+
+# # If_Else
+# # age=19
+# age=int(input("Enter your Age"))
+# if age>=19:
+#     print("Eligible for Vote")
+# else:
+#     print("Not Eligible for Vote")
+
+
+
+# #for loog
+# for i in range(1,6):
+#     print(i)
+
+
+# #while loop
+# i=1
+# while i<=5:
+#     print(i)
+#     i+=1
+    
+
+
+# #Function creation and Use
+# def greet(name):
+#     print("Hello",name)
+
+# greet("RT")
+# greet("Kush, Welcome to the World!")
+
+
+                                         ### PRACTICE BASIC ###
+
+# # Calculator
+# a=int(input("Enter the alue of A : "))
+# b=int(input("Enter the Value of B : "))
+
+# print("Sum: ",a+b)
+# print("Divide: ",a/b)
+# print("Multiply: ",a*b)
+# print("Substract: ",a-b)
+
+# #ODD & EVEN
+# n=int(input("Enter Any Number to Cheack ODD & EVEN: "))
+# if n%2!=0:
+#     print("This is Odd Number")
+# else:
+#     print("This is Even Number")
+
+# #Print Table 
+# n=int(input("Enter Any Number: "))
+# for i in range(1,11):
+#     print(n,"*",i, " = ",i*n)
+
+
